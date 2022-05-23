e36f7138197ef07f4006e935641dbf927f35d997
Kiran <42511218+sunnyblueskies32@users.noreply.github.com>
2022-05-24 09:12:56 +1000

    Update main.py

--- main.py
+++ main.py
@@ -243,5 +243,5 @@ def read_root():
     return Response(status_code=200)
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="127.0.0.1", port=8000)
